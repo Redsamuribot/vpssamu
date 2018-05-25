f89edf14b9bc7f1e1e5a72f37687be5ffd311ebf
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-05-25 22:45:50 +0700

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/2018.07332.2233.Tanduri/760.1.6 Tanysyz/18.0.0"
-    LA = "IOSIPAD\t7.18.0\tiPhone OS\t11.12.1"
+    UA = "Line/2018.07332.2233.Tanduri/760.1.6 Tanysyz/18.2.1"
+    LA = "DESKTOPMAC	8.5.2	HELLO-WORLD	12.13.2"
 
     authToken = None
     mid = None
